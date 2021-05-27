a56f16b9fe9cbfd9448747cf58d60e0eb0885511
theophilus <theophilus.ogbolu@gmail.com>
2021-05-27 12:29:13 +0100

    fix nav

--- src/routes/_3.svelte
+++ src/routes/_3.svelte
@@ -1,13 +1,19 @@
 <script>
 	import Section from './_section.svelte';
+	import CountDown from './_countDown.svelte';
+
+	let time = new Date('June 12, 2021 10:00:00').getTime();
 </script>
 
 <div id="three">
 	<Section bgi="date" let:bgi>
 		<img src="/image/date.gif" alt="date" />
 		<br />
-		<h1 class:v1={bgi != null}>December 17th, 2020</h1>
+		<h1 class:v1={bgi != null}>December 17th, 2020 - {time}</h1>
 		<h2>RCCG - Christ The Lord , 1-5 Admiralty Road, Lekki Phase 1</h2>
+		<br />
+		<br />
+		<CountDown {time} />
 	</Section>
 </div>
 
---
+++ src/routes/_countDown.svelte
@@ -0,0 +1,62 @@
+<script>
+	let days;
+	let hours;
+	let minutes;
+	let seconds;
+	let distance;
+
+	export let time = new Date('Dec 31, 2021 00:00:00').getTime();
+
+	const x = () => {
+		let now = new Date().getTime();
+		distance = time - now;
+
+		days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	};
+	setInterval(x, 1000);
+</script>
+
+<div class="count">
+	{#if distance > 0}
+		{#if days > 0}
+			<div class="grp">
+				<h2>{days}</h2>
+				<p>Day{days > 1 ? 's' : ''}</p>
+			</div>
+		{/if}
+		{#if hours > 0}
+			<div class="grp">
+				<h2>{hours}</h2>
+				<p>Hour{hours > 1 ? 's' : ''}</p>
+			</div>
+		{/if}
+		{#if minutes > 0}
+			<div class="grp">
+				<h2>{minutes}</h2>
+				<p>Minute{minutes > 1 ? 's' : ''}</p>
+			</div>
+		{/if}
+		<div class="grp">
+			<h2>{seconds}</h2>
+			<p>Second{seconds > 1 ? 's' : ''}</p>
+		</div>
+	{:else}
+		<h2>EXPIRED</h2>
+	{/if}
+</div>
+
+<style>
+	.count {
+		display: flex;
+		gap: 20px;
+		/* width: 100%;
+		max-width: 300px; */
+	}
+
+	h2 {
+		font-size: 3em;
+	}
+</style>
